e1aee19b53c97dfc718932de2f54663e7e626121
Henry Cleland <hcleland@companywatch.net>
2018-01-18 16:01:02 +0000

    working express node-js app simple playbook

--- node-app/app.js
+++ node-app/app.js
@@ -1,5 +1,5 @@
-var express = require('express'),
-app = express.createServer();
+var express = require('express');
+var app = express();
 
 app.get('/', function(req, res){
 	res.send('Hello World!');
